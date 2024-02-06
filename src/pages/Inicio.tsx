72283e7b7cf5dbd2e2c993f583ee650999a0af9b
libertyDv <libertytamayo0@gmail.com>
2024-02-06 10:55:12 +0100

    add botton and popup

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Inicio.css'
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
 
 function Inicio() {
+
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setPopupOpen(false);
+  };
+
   return (
     <div>
       <div className='divsInicio'>
@@ -21,14 +33,47 @@ function Inicio() {
       <div className='divTareas'>
         <div className='contenedorTarea'>
           <button className='botonDelete'>
-            <DeleteIcon/>
+            <DeleteIcon />
           </button>
           <button className='botonDone'>
-            <CheckIcon/>
+            <CheckIcon />
           </button>
           <p>Nombre de la tarea</p>
         </div>
       </div>
+
+      <button className='botonAdd' onClick={openPopup}>
+        <AddIcon />
+      </button>
+
+      {popupOpen && (
+        <div className='popup'>
+
+          <input
+            type='text'
+            placeholder='Nombre de la tarea'></input>
+          <br></br>
+
+          <input
+            type='text'
+            placeholder='Añadir subtarea'></input>
+          <br></br>
+
+          <input
+            type='date'
+            placeholder='Fecha límite'></input>
+          <br></br>
+
+          <label htmlFor="categoria">Categoria:</label>
+          <select id="categoria">
+            <option value="trabajo">Trabajo</option>
+            <option value="personal">Personal</option>
+            <option value="otra">Otra</option>
+          </select>
+
+          <button onClick={closePopup}>Cerrar</button>
+        </div>
+      )}
     </div>
   )
 }
